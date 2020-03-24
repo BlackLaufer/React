48c1e53bc9431d0ca0b3babfbfb9d861d2460dd6
Bett Baranovsky <black.laufer2000@gmail.com>
2020-03-24 17:37:20 +0100

    Add ESLint and husky - bugs fixed

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
-import React from "react";
-import styles from "./List.scss";
-import Hero from "../Hero/Hero";
+import React from 'react';
+import styles from './List.scss';
+import Hero from '../Hero/Hero';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
 import Creator from '../Creator/Creator.js';
@@ -13,14 +13,14 @@ class List extends React.Component {
 
   static propTypes = {
     title: PropTypes.node.isRequired,
-     description: PropTypes.node,
+    description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.string,
   };
 
   static defaultProps = {
     description: settings.defaultListDescription,
-  };
+  }
 
   addColumn(title) {
     this.setState(state => (
@@ -31,9 +31,9 @@ class List extends React.Component {
             key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
             title,
             icon: 'list-alt',
-            cards: []
-          }
-        ]
+            cards: [],
+          },
+        ],
       }
     ));
   }
@@ -46,15 +46,15 @@ class List extends React.Component {
           {this.props.description}
         </div>
         <div className={styles.columns}>
-        {this.state.columns.map(({key, ...columnProps}) => (
-          <Column key={key} {...columnProps} />
-        ))}
+          {this.state.columns.map(({key, ...columnProps}) => (
+            <Column key={key} {...columnProps} />
+          ))}
         </div>
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
       </section>
-    )
+    );
   }
 }
 
